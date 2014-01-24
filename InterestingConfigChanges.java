a91528cefb3d96ae36bf4f86f839aa148b50c3c0
Suprint <hadvlop@gmail.com>
2014-01-24 09:31:11 +0700

    File Java New Commit

---
+++ InterestingConfigChanges.java
@@ -0,0 +1,29 @@
+package com.mpay.ui.base;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.support.v4.content.pm.ActivityInfoCompat;
+
+/**
+ * Helper for determining if the configuration has changed in an interesting
+ * way so we need to rebuild the app list.
+ * @author quyenlm.vn@gmail.com
+ */
+public class InterestingConfigChanges {
+	final Configuration mLastConfiguration = new Configuration();
+	int mLastDensity;
+
+	boolean applyNewConfig(Resources res) {
+		int configChanges = mLastConfiguration.updateFrom(res
+				.getConfiguration());
+		boolean densityChanged = mLastDensity != res.getDisplayMetrics().densityDpi;
+		if (densityChanged
+				|| (configChanges & (ActivityInfo.CONFIG_LOCALE
+						| ActivityInfoCompat.CONFIG_UI_MODE | ActivityInfo.CONFIG_SCREEN_LAYOUT)) != 0) {
+			mLastDensity = res.getDisplayMetrics().densityDpi;
+			return true;
+		}
+		return false;
+	}
+}
